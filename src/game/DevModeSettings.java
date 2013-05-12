fed7083d0eb139ea0c4d4829f1ed957ff3f1c067
merotar <sebastian.artz@gmx.de>
2013-05-12 22:59:38 +0200

    added some graphics and missing classes

---
+++ src/game/DevModeSettings.java
@@ -0,0 +1,58 @@
+package game;
+
+/**
+ * Settings for the developer mode
+ * 
+ * @author Sebastian Artz
+ * @version 0.1
+ * @since 12-05-2013
+ * 
+ */
+public class DevModeSettings
+{
+	
+	static int maxActiveBlock = 7;
+//		static int maxActiveCollectable = 2;
+	static int activeBlock = 1;
+	static CollectableType activeCollectable = CollectableType.ABILITY_DIGGING;
+	
+	static void increaseActiveBlock()
+	{
+		activeBlock++;
+		if (activeBlock > maxActiveBlock) activeBlock = 0;
+	}
+	
+	static int getActiveBLock()
+	{
+		return activeBlock;
+	}
+	
+	static void increaseActiveCollectable()
+	{
+		if (activeCollectable == CollectableType.ABILITY_TELEPORT)
+		{
+			activeCollectable = CollectableType.BLOCK_BROWN;
+			return;
+		}
+		if (activeCollectable == CollectableType.BLOCK_BROWN)
+		{
+			activeCollectable = CollectableType.BLOCK_RED;
+			return;
+		}			
+		if (activeCollectable == CollectableType.BLOCK_RED)
+		{
+			activeCollectable = CollectableType.ABILITY_DIGGING;
+			return;
+		}
+		if (activeCollectable == CollectableType.ABILITY_DIGGING)
+		{
+			activeCollectable = CollectableType.ABILITY_TELEPORT;
+			return;
+		}
+	}
+	
+	static CollectableType getActiveCollectable()
+	{
+		return activeCollectable;
+	}
+}
